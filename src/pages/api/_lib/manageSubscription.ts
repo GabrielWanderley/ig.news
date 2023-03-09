3dd8484e92a51233a5ee45c62582fb8b6442621e
Gabriel Wanderley <gabrielwanderley08@gmail.com>
2023-03-09 11:30:05 -0300

    fauna

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -7,6 +7,7 @@ import { fauna } from './../../../services/fauna';
 export async function saveSubscription(
     subscriptionId: string,
     customerId: string,
+    createAction= false,
 ){
 
    const userRef = await fauna.query(
@@ -31,11 +32,29 @@ export async function saveSubscription(
     price_id:subscription.items.data[0].price.id,
    }
 
-   await fauna.query(
-    q.Create(
-        q.Collection('subscriptions'),
-        {data: subscriptionData}
+if (createAction){
+    await fauna.query(
+        q.Create(
+            q.Collection('subscriptions'),
+            {data: subscriptionData}
+        )
+       )
+}else{
+    await fauna.query(
+        q.Replace(
+            q.Select(
+                'ref',
+                q.Get(
+                    q.Match(
+                        q.Index('subscription_by_id'),
+                        subscriptionId
+                    )
+                )
+            ),
+            {data: subscriptionData}
+        )
     )
-   )
+
+}
 
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.update',
+    'customer.subscription.deleted'
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse)=>{
@@ -45,6 +47,18 @@ if (req.method === "POST"){
    if (relevantEvents.has(type)){
     try{
         switch(type){
+            case 'customer.subscription.update':
+            case 'customer.subscription.deleted':
+                const subscription = event.data.object as Stripe.Subscription
+                
+                await saveSubscription(
+                    subscription.id,
+                    subscription.customer.toString(),
+                    false
+                )
+                
+                break;
+            
             case 'checkout.session.completed':
          
            const checkoutSession = event.data.object as Stripe.Checkout.Session
